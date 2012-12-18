681e6da52995c25244e5529b3a8bf38a84de64d5
sunzhe <sz886@qq.com>
2012-12-17 22:47:42 -0800

    ddd

--- Traval100/AppDelegate.m
+++ Traval100/AppDelegate.m
@@ -21,6 +21,7 @@
     //测试 2
     //测试 3
     //测试 4
+    //1111
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
