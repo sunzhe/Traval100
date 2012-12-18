cfcc3d0065d2c1150fda3ed3e7ca160a48771307
sunzhe <sz886@qq.com>
2012-12-18 20:54:12 +0800

    321

--- Traval100/AppDelegate.m
+++ Traval100/AppDelegate.m
@@ -20,6 +20,7 @@
 {
     //ceshi 123
     //ceshi 456
+    //ceshi 321
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
