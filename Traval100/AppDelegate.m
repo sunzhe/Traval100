3309dcd909902545aa1d76a5b893e9f8db8ab8fd
sunzhe <sz886@qq.com>
2012-12-17 21:08:30 -0800

    测试3

--- Traval100/AppDelegate.m
+++ Traval100/AppDelegate.m
@@ -19,6 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     //测试 2
+    //测试 3
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
