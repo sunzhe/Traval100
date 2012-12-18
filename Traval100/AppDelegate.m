ae97d13dd9192f68191b84e048c1f000a5a0cb9c
sunzhe <sz886@qq.com>
2012-12-17 23:53:10 -0800

    靠删除测试终于成功了

--- Traval100/AppDelegate.m
+++ Traval100/AppDelegate.m
@@ -18,10 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    //测试 2
-    //测试 3
-    //测试 4
-    //1111
+    //靠 删除测试终于成功了
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
