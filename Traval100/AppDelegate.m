ed69f8b8d87a9ccd6f1eb87bba03c58e31e8e081
sunzhe <sz886@qq.com>
2012-12-18 21:32:53 +0800

    Revert "测试"
    This reverts commit ce44830fdf6efdf8deabff63830b331436bc397f.

--- Traval100/AppDelegate.m
+++ Traval100/AppDelegate.m
@@ -18,7 +18,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    //git 重新测试 
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
@@ -28,7 +27,7 @@
     [self.window addSubview:nav.view];
     [self.window makeKeyAndVisible];
     return YES;
-    //git 测试2
+    ////
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
